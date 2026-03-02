8d3dd3485e02df16ad04aec492a9a52fc8412ed2
Gabriel Carvalho <gabrielcdbarbosa@gmail.com>
2026-03-02 12:16:27 -0300

    criamos a classe Usuario

--- class/Sql.php
+++ class/Sql.php
@@ -1,6 +1,6 @@
 <?php
 
-class Sql extends PDO {
+class Sql {
 
     private $conn;
 
@@ -10,19 +10,19 @@ class Sql extends PDO {
 
     }
 
-    private function setParams($statment, $parameters = array()) {
+    private function setParams($statement, $parameters = array()) {
 
         foreach ($parameters as $key => $value) {
 
-            $this->setParam($key, $value);
+            $this->setParam($statement, $key, $value);
 
         }
 
     }
 
-    private function setParam($statment, $key, $value){
+    private function setParam($statement, $key, $value){
 
-        $statment->bindParam($key, $value);
+        $statement->bindParam($key, $value);
 
     }
 
