8a5d7dc26f1c347bab22dfb9ac468e92ea4358ca
Gabriel Carvalho <gabrielcdbarbosa@gmail.com>
2026-03-07 17:18:00 -0300

    comando delete

--- index.php
+++ index.php
@@ -28,7 +28,8 @@ $aluno = new Usuario("aluno", "@lun0");
 $aluno->insert();
 echo $aluno;
 */
-
+/*
+//Alterar um usuario
 $usuario = new Usuario();
 
 $usuario->loadById(8);
@@ -36,6 +37,14 @@ $usuario->loadById(8);
 $usuario->update("professor", "!@#$%''&*");
 
 echo $usuario;
+*/
 
+$usuario = new Usuario();
+
+$usuario->loadById(7);
+
+$usuario->delete();
+
+echo $usuario;
 
  ?>
